2523fb3fb83565a24139eca9d52c061c36401d1f
Emil Refn <emr@netcompany.com>
2019-10-21 15:26:30 +0200

    Added XML-comments to interfaces

--- ServiceBroker.Example.Core/IDynamicService.cs
+++ ServiceBroker.Example.Core/IDynamicService.cs
@@ -1,5 +1,8 @@
 ﻿namespace ServiceBroker.Example.Core
 {
+    /// <summary>
+    /// An external service wrapper, where the external service should be called every time wrapper is called.
+    /// </summary>
     public interface IDynamicService : IServiceBase
     {
     }
--- ServiceBroker.Example.Core/ITaskScheduler.cs
+++ ServiceBroker.Example.Core/ITaskScheduler.cs
@@ -3,8 +3,15 @@ using System.Threading;
 
 namespace ServiceBroker.Example.Core
 {
+    /// <summary>
+    /// An interface that represents any service that is able to schedule an action on a background thread.
+    /// </summary>
     public interface ITaskScheduler
     {
+        /// <summary>
+        /// Executes the specified action on a background thread.
+        /// </summary>
+        /// <param name="workItem">The action to be executed on the background thread.</param>
         void QueueBackgroundWorkItem(Action<CancellationToken> workItem);
     }
 }
